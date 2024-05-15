d3a5650b5c2877ad9decfcc0f931f56ad39489cc
seongmin221 <seongmin221@naver.com>
2024-05-15 19:56:41 +0900

    initial commit

---
+++ APIClientSample/APIClientSample/Data/EndpointType.swift
@@ -0,0 +1,8 @@
+//
+//  EndpointType.swift
+//  APIClientSample
+//
+//  Created by 이성민 on 5/15/24.
+//
+
+import Foundation
---
+++ APIClientSample/APIClientSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIClientSample
+//
+//  Created by 이성민 on 5/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
