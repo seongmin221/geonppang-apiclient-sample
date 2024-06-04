5813a866a97d753098a54429a64b71d5ee732446
seongmin221 <seongmin221@naver.com>
2024-06-04 15:06:22 +0900

    added urlrequest extension for infrastructure

--- APIClientSample/APIClientSample/Infrastructure/Network/URLRequest+Encoding.swift
+++ APIClientSample/APIClientSample/Infrastructure/Network/URLRequest+Encoding.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+extension URLRequest {
+    func encode(queries: [String: Any]) throws -> URLRequest {
+        guard var url = self.url else {
+            throw GBNetworkError.requestEncodingFailure(.pathError("missing url"))
+        }
+        url.append(queryItems: queries.map(toQueryItem))
+        
+        return .init(url: url)
+    }
+    
+    func encode(body: Encodable) throws -> URLRequest {
+        do {
+            var request = self
+            request.httpBody = try JSONEncoder().encode(body)
+            return request
+        } catch {
+            throw GBNetworkError.requestEncodingFailure(.jsonError("incorrect body type"))
+        }
+    }
+}
+
+private extension URLRequest {
+    func toQueryItem(_ query: (key: String, value: Any)) -> URLQueryItem {
+        .init(name: query.key, value: query.value as? String)
+    }
+}
